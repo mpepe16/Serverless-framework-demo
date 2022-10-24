cdddf683370920ab586a79f898393438f1366b96
mpepe16 <mpepe66@gmail.com>
2022-10-24 16:16:01 +0200

    Add: basic dynamodb for get some data.

---
+++ serverlessDemoMp/lambdas/endpoints/getPlayerScore.ts
@@ -0,0 +1,26 @@
+import { Responses } from'../common/API_Responses';
+import { Dynamo} from '../common/Dynamo';
+
+const tableName = process.env.tableName;
+
+exports.handler = async (event: { pathParameters: { ID: any; }; }) => {
+    console.log('event', event);
+
+    if (!event.pathParameters || !event.pathParameters.ID) {
+        // failed without an ID
+        return Responses._400({ message: 'missing the ID from the path' });
+    }
+
+    let ID = event.pathParameters.ID;
+
+    const user = await Dynamo.get(ID, tableName).catch((err: any) => {
+        console.log('error in Dynamo Get', err);
+        return null;
+    });
+
+    if (!user) {
+        return Responses._400({ message: 'Failed to get user by ID' });
+    }
+
+    return Responses._200({ user });
+};
